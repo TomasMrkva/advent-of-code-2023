a6b5e9dfea9e17a495b87e0433d8e8d6b39d8c0a
TomasMrkva <57423011+TomasMrkva@users.noreply.github.com>
2023-12-09 22:17:57 +0000

    d9 challenges

---
+++ day9/d9ptB.py
@@ -0,0 +1,25 @@
+with open("input.txt", "r") as f:
+    lines = f.read().splitlines()
+
+def get_differences(nums: list[int]) -> list[int]:
+    return [*map(lambda x, y: y - x, nums, nums[1:])]
+
+def repeat(nums: list[int]) -> list[list[int]]:
+    diffs = get_differences(nums)
+    if all(d == 0 for d in diffs):
+        return [diffs]
+    return repeat(diffs) + [diffs]
+
+def interpolate(nums: list[list[int]], idx: int):
+    if idx == len(nums): return nums
+    nxt = nums[idx][0] - nums[idx-1][0]
+    nums[idx].insert(0, nxt)
+    return interpolate(nums, idx + 1)
+
+def sol(lines: list[str]):
+    for l in lines:
+        nums = [*map(int, l.split())]
+        diffs = repeat(nums) + [nums]
+        yield interpolate(diffs, 1)[-1][0]
+
+print(sum(sol(lines)))
