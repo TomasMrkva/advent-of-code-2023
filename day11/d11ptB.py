2b4a8178679246bca86c9515e7899f57f8b8434c
TomasMrkva <57423011+TomasMrkva@users.noreply.github.com>
2023-12-11 23:13:15 +0000

    added day 11

--- day11/d11ptB.py
+++ day11/d11ptB.py
@@ -1,21 +1,20 @@
 f = open("input.txt", "r")
 ls = f.read().splitlines()
 
-gap = 1000000 - 1
+row_gaps = []
+col_gaps = []
+gap = 1000000
 
 def expand_rows(lines: list[str]) -> list[str]:
-    for l in lines:
+    for i,l in enumerate(lines):
         if not '#' in l:
-            yield from ('.' * len(lines[0]) for _ in range(gap))
+            row_gaps.append(i)
         yield l
 
 def expand_cols(lines: list[str]) -> list[str]:
-    i = 0
-    while i < len(lines[0]):
+    for i in range(len(lines[0])):
         if not '#' in [lines[x][i] for x in range(len(lines))]:
-            lines = [lines[x][:i] + '.' * gap + lines[x][i:] for x in range(len(lines))]
-            i += gap
-        i += 1
+            col_gaps.append(i)
     return lines
 
 def get_stars_pos(lines: list[str]):
@@ -27,11 +26,22 @@ def get_all_combs(stars: list[tuple[int]]):
         yield from [(el, x) for x in stars[:i] + stars[i+1:]]
         
 def get_manhattan_diff(pair) -> int:
-    x, y = pair
-    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+    a, b = pair
+    mul_rows = 0
+    mul_cols = 0
+    
+    for i in range(min(a[0], b[0]), max(a[0], b[0])):
+        if i in row_gaps:
+            mul_rows += 1
 
-x = expand_cols(list(expand_rows(ls)))
+    for i in range(min(a[1], b[1]), max(a[1], b[1])):
+        if i in col_gaps:
+            mul_cols += 1
+    
+    return abs(a[0] - b[0]) - mul_rows + mul_rows * gap + abs(a[1] - b[1]) - mul_cols + mul_cols * gap
+
 sky = expand_cols(list(expand_rows(ls)))
 combs = get_all_combs(list(get_stars_pos(sky)))
 res = sum(list(map(get_manhattan_diff, combs))) // 2
+
 print(res)
